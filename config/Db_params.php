de039265539cae5f3f45829a3d14d2cf0c0b1da2
Gremosly <seruy1432@gmail.com>
2016-02-21 19:57:52 +0200

    Added files via upload

---
+++ config/Db_params.php
@@ -0,0 +1,14 @@
+<?php
+
+	return array(
+		'host' => 'sql207.byethost7.com',
+		'dbname' => 'b7_17445822_phpshop',
+		'user' => 'b7_17445822',
+		'password' => 'shutnik1999',
+		'charset' => 'utf8'
+	);
+
+
+
+
+?>
